4ddf611e0d8cd69cff26c111c42da875750a597e
Bainda <giorgibainda14@gmail.com>
2025-01-18 14:57:45 +0400

    Changed loading spinner and adjusted it to logo for better UX and also added custom 404 page.

--- src/app/core/components/not-found/not-found.component.ts
+++ src/app/core/components/not-found/not-found.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css'],
-  standalone: true
+  selector: "app-not-found",
+  templateUrl: "./not-found.component.html",
+  styleUrls: ["./not-found.component.scss"],
+  imports: [RouterLink],
+  standalone: true,
 })
 export class NotFoundComponent {
-
-  constructor() { }
-
+  constructor() {}
 }
